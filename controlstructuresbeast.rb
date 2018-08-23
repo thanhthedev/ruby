8550629721fcfc9a2d862c780e791fa6f87f42e7
thanhthedev <huythanhdao@gmail.com>
2018-08-23 15:51:12 +1000

    beast challenge

---
+++ controlstructuresbeast.rb
@@ -0,0 +1,9 @@
+
+puts "Welcome to the banking app. What would you like to do? (options: balance)"
+    userinput = gets.chomp
+    if (userinput == 'balance') 
+        puts "Your balance is $0"
+
+    else puts "Invalid Selection"
+
+    end
